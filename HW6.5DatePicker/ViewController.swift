51785866c78cd8ade014d0b9310f015008483e03
Daniel <daniel@macbook-pro.local>
2018-07-21 23:34:32 +0800

    first commit

--- HW6.5DatePicker/ViewController.swift
+++ HW6.5DatePicker/ViewController.swift
@@ -13,8 +13,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+//        let now = Date()
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "yyyy MM dd"
+//        let time = formatter.string(from: now)
+//        nowTime.text = time
     }
 
 
+    @IBOutlet weak var goalTime: UITextField!
+    @IBOutlet weak var nowTime: UITextField!
+    @IBOutlet weak var intervalTime: UITextField!
+    
+   // var goal = Date!
+    @IBAction func timePicker(_ sender: UIDatePicker) {
+        
+     let goal = sender.date
+     let now = Date()
+     let formatter = DateFormatter()
+     formatter.dateFormat = "yyyy年MM月dd日"
+     let time = formatter.string(from: now)
+     nowTime.text = time
+     let goalDate = formatter.string(from: goal)
+     goalTime.text = goalDate
+        let interval = goal.timeIntervalSince(now)
+        let integerinterval = Int(interval/60/60/24+1)
+        intervalTime.text = ("\(integerinterval)")
+
+    }
+    
 }
 
